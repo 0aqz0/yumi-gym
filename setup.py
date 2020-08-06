08edd433dfe293704a0db069e0b2e29eb22b8bb4
0aqz0 <18026332256@163.com>
2020-08-06 14:25:43 +0800

    setup with non-code files

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
 from pathlib import Path
+import os
+
+directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'yumi_gym', 'envs', 'assets')
+data_files = []
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        data_files.append(os.path.join(root, file))
 
 setup(
     name='yumi-gym',
-    version='0.0.1',
+    version='0.0.3',
     packages=find_packages(),
+    package_data={'yumi_gym': data_files},
+    include_package_data=True,
     install_requires=['gym', 'pybullet', 'numpy'],
     description="Physics simulation for ABB's collaborative robot yumi",
-    long_description_content_type="text/markdown",
     long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     url='https://github.com/0aqz0/yumi-gym',
     author='Haodong Zhang',
     author_email='aqz@zju.edu.cn',
